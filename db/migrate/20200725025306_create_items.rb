25cf82ab323684557b17084880454adfdff89797
yuuki0114 <y.no0114@gmail.com>
2020-08-01 15:25:36 +0900

    Update 20200725025306_create_items.rb

--- db/migrate/20200725025306_create_items.rb
+++ db/migrate/20200725025306_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string  :name,          null: false
       t.text    :detail,        null: false
-      t.string  :rental_days,   null: false
+      t.string  :rental_period, null: false
       t.integer :price,         null: false
       t.string  :image,         null: false
       t.timestamps
