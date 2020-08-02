9ff0ee2668dedef049b27255ee92cff27e5ab268
yuuki0114 <y.no0114@gmail.com>
2020-08-03 06:48:43 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name,          null: false
+    validates :detail,        null: false
+    validates :image,         null: false
+    validates :rental_period, null: false
+    validates :price,         null: false
+  end
 end
