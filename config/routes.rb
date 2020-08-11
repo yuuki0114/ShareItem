f851c5b68d561bd16e6cfbcb9e446a4f28a2a073
yuuki0114 <y.no0114@gmail.com>
2020-08-11 20:09:43 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
+  resources :users, only: [:index]
   resources :items, only: [:new, :create, :show]
+  resources :credit_cards, only: [:new, :create] 
 end
