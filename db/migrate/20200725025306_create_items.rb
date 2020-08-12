d3bf665191e1b57d3cd1c342bd6f773ff12edf70
yuuki0114 <y.no0114@gmail.com>
2020-08-12 15:04:29 +0900

    Update 20200725025306_create_items.rb

--- db/migrate/20200725025306_create_items.rb
+++ db/migrate/20200725025306_create_items.rb
@@ -1,11 +1,14 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string  :name,          null: false
-      t.text    :detail,        null: false
-      t.string  :rental_period, null: false
-      t.integer :price,         null: false
-      t.string  :image,         null: false
+      t.string  :name,             null: false
+      t.text    :detail,           null: false
+      t.string  :rental_period,    null: false
+      t.integer :price,            null: false
+      t.string  :image,            null: false
+      t.integer :postcode,         null: false
+      t.string  :address_region,   null: false
+      t.string  :trading_status,   null: false
       t.timestamps
     end
   end
