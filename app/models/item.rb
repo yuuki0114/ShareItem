543a4d7eb89cc269b322cf91db75a26439f8e9f6
yuuki0114 <y.no0114@gmail.com>
2020-08-12 15:05:48 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
   with_options presence: true do
-    validates :name,          null: false
-    validates :detail,        null: false
-    validates :image,         null: false
-    validates :rental_period, null: false
-    validates :price,         null: false
-    validates :postcode,      null: false
+    validates :name,           null: false
+    validates :detail,         null: false
+    validates :image,          null: false
+    validates :rental_period,  null: false
+    validates :price,          null: false
+    validates :postcode,       null: false
+    validates :address_region, null: false
+    validates :trading_status, null: false
   end
 
   mount_uploader :image, ImageUploader 
