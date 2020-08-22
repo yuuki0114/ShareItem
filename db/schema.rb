fd5316cfc5424fe058aae415c6cacfd6042f35d5
yuuki0114 <y.no0114@gmail.com>
2020-08-22 13:05:23 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_100210) do
+ActiveRecord::Schema.define(version: 2020_08_18_223309) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_08_14_100210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["name"], name: "index_items_on_name"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
